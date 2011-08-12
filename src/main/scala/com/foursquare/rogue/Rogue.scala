95369cd4573cb5287ab0ca06989f270063fde472
Benjy <benjy@foursquare.com>
2011-08-11 18:05:33 -0700

    Minor type nits.

--- src/main/scala/com/foursquare/rogue/Rogue.scala
+++ src/main/scala/com/foursquare/rogue/Rogue.scala
@@ -11,10 +11,12 @@ import net.liftweb.mongodb.record.field.{BsonRecordField, BsonRecordListField, M
 import org.bson.types.ObjectId
 
 trait Rogue {
+  type Command[T] = com.foursquare.rogue.Command[T]
+
   type Query[M <: MongoRecord[M]] = BasicQuery[M, M, Unordered, Unselected, Unlimited, Unskipped, HasNoOrClause]
   type OrderedQuery[M <: MongoRecord[M]] = BasicQuery[M, M, Ordered, Unselected, Unlimited, Unskipped, HasNoOrClause]
-  type PaginatedQuery[T <: MongoRecord[T]] = com.foursquare.rogue.PaginatedQuery[T, T]
-  type ModifyQuery[T <: MongoRecord[T]] = com.foursquare.rogue.ModifyQuery[T]
+  type PaginatedQuery[M <: MongoRecord[M]] = com.foursquare.rogue.PaginatedQuery[M, M]
+  type ModifyQuery[M <: MongoRecord[M]] = com.foursquare.rogue.ModifyQuery[M]
 
 
   // Convenient shorthand.
