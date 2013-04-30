8b9014370571973016af8d2cade7375b80c6ac95
Tom Dyas <tdyas@foursquare.com>
2013-04-30 17:03:42 -0400

    extract index declarations into rogue-index
    Extract the index declaration code into a new artifact: rogue-index.
    This makes it convenient to refer to the index declaration code without
    depending on rogue-core.

--- rogue-core/src/main/scala/com/foursquare/rogue/index/IndexChecker.scala
+++ rogue-core/src/main/scala/com/foursquare/rogue/index/IndexChecker.scala
@@ -3,13 +3,6 @@ package com.foursquare.rogue.index
 
 import com.foursquare.rogue.{DocumentScan, Index, IndexScan, PartialIndexScan, MongoHelpers, Query, QueryClause, QueryHelpers}
 
-/**
- * A trait that represents the fact that a record type includes a list
- * of the indexes that exist in MongoDB for that type.
- */
-trait IndexedRecord[M] {
-  val mongoIndexList: List[MongoIndex[_]] = List()
-}
 
 /**
  * A utility object which provides the capability to verify if the set of indexes that
