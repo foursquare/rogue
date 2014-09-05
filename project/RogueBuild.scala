41e4de4bc8a9370ef6baf91d4f75a7944b6c043f
Jason Liszka <jliszka@alumni.cmu.edu>
2014-09-05 12:32:46 -0400

    3.0.0-beta8 release

--- project/RogueBuild.scala
+++ project/RogueBuild.scala
@@ -16,7 +16,7 @@ object RogueBuild extends Build {
   lazy val IvyDefaultConfiguration = config("default") extend(Compile)
 
   lazy val defaultSettings: Seq[Setting[_]] = Seq(
-    version := "3.0.0-beta7a",
+    version := "3.0.0-beta8",
     organization := "com.foursquare",
     crossScalaVersions := Seq("2.9.1", "2.9.2", "2.10.2"),
     publishMavenStyle := true,
